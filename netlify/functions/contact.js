b3544bdb3a3e59f64d983a9e4d6d01cee18cf6bc
Sabin Maharjan <samah013@kathmandu>
2023-08-22 14:39:43 +0545

    contact api refactor

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -2,16 +2,31 @@ const mongoose = require("mongoose");
 
 exports.handler = async (event) => {
   try {
-    mongoose.connect("mongodb://localhost:27017/contact-form", {
+    // Connect to the MongoDB database
+    await mongoose.connect("mongodb://localhost:27017/contact-form", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    const db = mongoose.connection;
     // Define a contact schema
+    const contactSchema = new mongoose.Schema({
+      name: String,
+      email: String,
+      message: String,
+    });
+
+    // Create a model using the schema
+    const Contact = mongoose.model("Contact", contactSchema);
+
+    // Retrieve all contacts from the database
+    const contacts = await Contact.find();
+
+    // Close the database connection
+    mongoose.connection.close();
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Function executed successfully." }),
+      body: JSON.stringify(contacts),
     };
   } catch (error) {
     return {
