82b81bcbe1503bc4127c67d88842914bafdbedf0
Sabin Maharjan <samah013@kathmandu>
2023-08-22 12:28:37 +0545

    contact db return as response

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -8,11 +8,30 @@ exports.handler = async (event) => {
     });
 
     const db = mongoose.connection;
-    return {
-      statusCode: 200, // Correct status code
-      body: JSON.stringify({ message: "Data fetched successfully" }),
-    };
-    // Rest of your contacts route logic...
+
+    // Ensure the connection is open before querying
+    await db.once("open", async () => {
+      // Define a contact schema
+      const contactSchema = new mongoose.Schema({
+        name: String,
+        email: String,
+        message: String,
+      });
+
+      const Contact = mongoose.model("Contact", contactSchema);
+
+      // Query all contacts
+      const contacts = await Contact.find();
+
+      // Close the MongoDB connection
+      db.close();
+
+      // Return the fetched contacts
+      return {
+        statusCode: 200,
+        body: JSON.stringify(contacts),
+      };
+    });
   } catch (error) {
     return {
       statusCode: 500,
