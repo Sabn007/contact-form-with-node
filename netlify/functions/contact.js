cf1d4b0cbca207129d3a9c6aff1e036ebbab165e
Sabin Maharjan <samah013@kathmandu>
2023-08-22 12:41:33 +0545

    contact db return as response try 2

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -2,53 +2,33 @@ const mongoose = require("mongoose");
 
 exports.handler = async (event) => {
   try {
-    mongoose.connect("mongodb://localhost:27017/contact-form", {
+    mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
     const db = mongoose.connection;
 
-    db.on("error", (error) => {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({
-          error: "An error occurred while connecting to the database.",
-        }),
-      };
+    const contactSchema = new mongoose.Schema({
+      name: String,
+      email: String,
+      message: String,
     });
 
-    db.once("open", async () => {
-      const contactSchema = new mongoose.Schema({
-        name: String,
-        email: String,
-        message: String,
-      });
+    const Contact = mongoose.model("Contact", contactSchema);
 
-      const Contact = mongoose.model("Contact", contactSchema);
+    const contacts = await Contact.find();
+    db.close();
 
-      try {
-        const contacts = await Contact.find();
-        db.close();
-
-        return {
-          statusCode: 200,
-          body: JSON.stringify(contacts),
-        };
-      } catch (error) {
-        return {
-          statusCode: 500,
-          body: JSON.stringify({
-            error: "An error occurred while fetching contacts.",
-          }),
-        };
-      }
-    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(contacts),
+    };
   } catch (error) {
     return {
       statusCode: 500,
       body: JSON.stringify({
-        error: "An error occurred while connecting to the database.",
+        error: "An error occurred while fetching contacts.",
       }),
     };
   }
